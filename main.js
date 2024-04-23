9cec108117751554e2526b67290256908389c7d8
leonardo271516 <140813584+leonardo271516@users.noreply.github.com>
2024-04-23 11:52:25 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+const botoes = document.querySelectorAll(".botao");
+
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+
+    botoes[i].onclick = function(){
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+
+        }
+
+        }
+
+        const contadores = document.querySelectorAll(".contador");
+const tempoAtual = new Date(); // retorna a data atual do computador
+const tempoObjetivo1 = new Date("2024-05-02T00:00:00");
+
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+
+let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+console.log("faltam" + dias, "dias" + horas, "horas" + minutos, "minutos");
+
+contadores[0].textContent = "faltam " + dias+ " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos " ;
